c40c8be1c0fda88ff5b0d17b7b71533dcb10dbd1
Peter Law <PeterJCLaw@gmail.com>
2017-03-12 18:27:57 +0000

    Add scoring logic and tests
    Change-Id: If5f3baa5711bb37981c9227a49cf05b1d9e0f9d1

--- scoring/score.py
+++ scoring/score.py
@@ -1,12 +1,59 @@
 
+from collections import Counter
+
+class InvalidScoresheetException(Exception):
+    pass
+
 class Scorer:
-    def __init__(self, teams_data, arena_data = None):
-        self._teams_data = teams_data,
-        self._arena_data = arena_data or {}
+    VALID_TOKENS = 'ABC'
+
+    def __init__(self, teams_data, arena_data):
+        self._teams_data = teams_data
+        self._arena_data = arena_data
 
     def calculate_scores(self):
-        scores = {tla: 0 for tla in self._scoresheet.keys()}
+        scores = {}
+        for tla, info in self._teams_data.items():
+            zone = info['zone']
+            tokens = self._arena_data[zone]['tokens']
+
+            # 1 point per token
+            points = len(tokens)
+
+            counts = Counter(tokens)
+
+            if 'A' in counts and 'B' in counts:
+                # 1 further point per B token if there's an A token
+                points += counts['B']
+
+                # 2 further points per C token if there are both A and B tokens
+                points += 2 * counts.get('C', 0)
+
+            scores[tla] = points
+
         return scores
 
     def validate(self, extra):
-        pass
+        all_tokens = ''.join(
+            d['tokens']
+            for d in self._arena_data.values()
+        ).replace(' ', '')
+
+        num_tokens = len(all_tokens)
+        if not num_tokens == 9:
+            msg = "Should have exactly 9 tokens (got {0})".format(num_tokens)
+            raise InvalidScoresheetException(msg)
+
+        valid_tokens = set(self.VALID_TOKENS)
+        actual_tokens = set(all_tokens)
+        extras = actual_tokens - valid_tokens
+        if extras:
+            extras_str = ', '.join(extras)
+            valid_str =  ', '.join(valid_tokens)
+            msg = "Found invalid tokens {0} (valid: {1})".format(extras_str, valid_str)
+            raise InvalidScoresheetException(msg)
+
+
+if __name__ == '__main__':
+    import libproton
+    libproton.main(Scorer)
---
+++ scoring/tests/test_abc_scoring.py
@@ -0,0 +1,353 @@
+
+from score import Scorer
+
+# Path hackery
+import os.path
+import sys
+ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, ROOT)
+
+from score import Scorer, InvalidScoresheetException
+
+def test_scores_match_start():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2},
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': '' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AAAA BBBB C' },
+    }
+    expected = {
+        'ABC': 0,
+        'DEF': 0,
+        'GHI': 0,
+    }
+
+    scorer = Scorer(teams_data, arena_data)
+    scores = scorer.calculate_scores()
+
+    assert expected == scores, "Wrong scores!"
+
+def test_scores_one_zone_1():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2},
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': '' },
+        2: { 'tokens': 'ABA' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AA BBB C' },
+    }
+    expected = {
+        'ABC': 0,
+        'DEF': 0,
+        'GHI': 4,
+    }
+
+    scorer = Scorer(teams_data, arena_data)
+    scores = scorer.calculate_scores()
+
+    assert expected == scores, "Wrong scores!"
+
+def test_scores_one_zone_2():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2}
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'AC' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AAA BBBB C' },
+    }
+    expected = {
+        'ABC': 0,
+        'DEF': 2,
+        'GHI': 0,
+    }
+
+    scorer = Scorer(teams_data, arena_data)
+    scores = scorer.calculate_scores()
+
+    assert expected == scores, "Wrong scores!"
+
+def test_scores_combined():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2}
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'AC' },
+        2: { 'tokens': 'AAB' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'A BBB' },
+    }
+    expected = {
+        'ABC': 0,
+        'DEF': 2,
+        'GHI': 4,
+    }
+
+    scorer = Scorer(teams_data, arena_data)
+    scores = scorer.calculate_scores()
+
+    assert expected == scores, "Wrong scores!"
+
+def test_scores_no_A():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2}
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'BBC' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AAAA BB' },
+    }
+    expected = {
+        'ABC': 0,
+        'DEF': 3,
+        'GHI': 0,
+    }
+
+    scorer = Scorer(teams_data, arena_data)
+    scores = scorer.calculate_scores()
+
+    assert expected == scores, "Wrong scores!"
+
+def test_scores_AC_no_B():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2}
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'AC' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AAA BBBB' },
+    }
+    expected = {
+        'ABC': 0,
+        'DEF': 2,
+        'GHI': 0,
+    }
+
+    scorer = Scorer(teams_data, arena_data)
+    scores = scorer.calculate_scores()
+
+    assert expected == scores, "Wrong scores!"
+
+def test_validate_error_invalid_notation():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2},
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'A' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'W' * 8 },
+    }
+
+    try:
+        scorer = Scorer(teams_data, arena_data)
+        scores = scorer.validate(None)
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when there are invalid tokens"
+
+
+def test_validate_error_too_few_tokens():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2},
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'A' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'B C' },
+    }
+
+    try:
+        scorer = Scorer(teams_data, arena_data)
+        scores = scorer.validate(None)
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when there are too few tokens"
+
+def test_validate_error_too_many_tokens():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2},
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'AA BB C' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AAA BBB C' },
+    }
+
+    try:
+        scorer = Scorer(teams_data, arena_data)
+        scores = scorer.validate(None)
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when there are too many tokens"
+
+def test_validate_error_too_many_A_tokens():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2},
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'AA BB C' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AAA BB' },
+    }
+
+    try:
+        scorer = Scorer(teams_data, arena_data)
+        scores = scorer.validate(None)
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when there are too many A tokens"
+
+def test_validate_error_too_many_B_tokens():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2},
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'AA BB C' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AA BBB' },
+    }
+
+    try:
+        scorer = Scorer(teams_data, arena_data)
+        scores = scorer.validate(None)
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when there are too many B tokens"
+
+def test_validate_error_too_many_C_tokens():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2},
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'AA BB C' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AA BB C' },
+    }
+
+    try:
+        scorer = Scorer(teams_data, arena_data)
+        scores = scorer.validate(None)
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when there are too many C tokens"
+
+def test_validate_error_too_few_A_tokens():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2},
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'A BB C' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AA BB' },
+    }
+
+    try:
+        scorer = Scorer(teams_data, arena_data)
+        scores = scorer.validate(None)
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when there are too few A tokens"
+
+def test_validate_error_too_few_B_tokens():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2},
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'AA BB C' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AA B' },
+    }
+
+    try:
+        scorer = Scorer(teams_data, arena_data)
+        scores = scorer.validate(None)
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when there are too few B tokens"
+
+def test_validate_error_too_few_C_tokens():
+    teams_data = {
+        'ABC': {'zone': 0},
+        'DEF': {'zone': 1},
+        'GHI': {'zone': 2},
+    }
+    arena_data = {
+        0: { 'tokens': '' },
+        1: { 'tokens': 'AA BB' },
+        2: { 'tokens': '' },
+        3: { 'tokens': '' },
+        'other': { 'tokens': 'AA BB' },
+    }
+
+    try:
+        scorer = Scorer(teams_data, arena_data)
+        scores = scorer.validate(None)
+    except InvalidScoresheetException:
+        pass
+    else:
+        assert False, "Should error when there are too few C tokens"
